remyers/signet2

add testing framework for simulator

test/functional/simulate_eltoo.py
@@ -1,0 +1,249 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015-2019 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+Simulation tests for eltoo payment channel update scheme
+"""
+
+import copy
+from test_framework.blocktools import (
+    create_block,
+    create_coinbase
+)
+from test_framework.key import ECKey, ECPubKey, SECP256K1_ORDER
+from test_framework.messages import (
+    COutPoint,
+    CScriptWitness,
+    CTransaction,
+    CTxIn,
+    CTxInWitness,
+    CTxOut,
+)
+from test_framework.mininode import P2PDataStore
+from test_framework.script import (
+    CScript,
+    CScriptNum,
+    OP_0,
+    OP_1, 
+    OP_2,
+    OP_2DUP,
+    OP_3DUP,
+    OP_2DROP,
+    OP_CHECKLOCKTIMEVERIFY, 
+    OP_CHECKMULTISIG,
+    OP_CHECKMULTISIGVERIFY,
+    OP_CHECKSEQUENCEVERIFY,
+    OP_CHECKSIG,
+    OP_CHECKSIGVERIFY,
+    OP_DROP,
+    OP_DUP,
+    OP_ELSE,
+    OP_ENDIF,
+    OP_EQUAL,
+    OP_EQUALVERIFY,
+    OP_FALSE,
+    OP_HASH160,
+    OP_IF,
+    OP_INVALIDOPCODE,
+    OP_NOTIF,
+    OP_RETURN,
+    OP_TRUE,
+    SIGHASH_ALL,
+    SIGHASH_ANYPREVOUT,
+    SIGHASH_NONE,
+    SIGHASH_SINGLE,
+    SIGHASH_ALLINPUT,
+    SIGHASH_ANYONECANPAY,
+    SIGHASH_ANYPREVOUT,
+    SIGHASH_NOINPUT,
+    SIGHASH_ANYPREVOUTANYSCRIPT,
+    SegwitVersion1SignatureHash,
+    SignatureHash,
+    hash160,
+    hash256,
+    sha256
+)
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    bytes_to_hex_str,
+)
+import time
+import random
+
+NUM_OUTPUTS_TO_COLLECT = 33
+CSV_DELAY = 20
+DUST_LIMIT = 800
+FEE_AMOUNT = 1000
+CHANNEL_AMOUNT = 1000000
+RELAY_FEE = 100
+NUM_SIGNERS = 2
+CLTV_START_TIME = 500000000
+INVOICE_TIMEOUT = 60 # 1 minute
+
+def int_to_bytes(x) -> bytes:
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+class SimulateL2Tests(BitcoinTestFramework):
+
+    def next_block(self, number, spend=None, additional_coinbase_value=0, script=CScript([OP_TRUE]), solve=True, *, version=1):
+        if self.tip is None:
+            base_block_hash = self.genesis_hash
+            block_time = int(self.start_time) + 1
+        else:
+            base_block_hash = self.tip.sha256
+            block_time = self.tip.nTime + 1
+        # First create the coinbase
+        height = self.block_heights[base_block_hash] + 1
+        coinbase = create_coinbase(height, self.coinbase_pubkey)
+        coinbase.vout[0].nValue += additional_coinbase_value
+        coinbase.rehash()
+        if spend is None:
+            block = create_block(base_block_hash, coinbase, block_time, version=version)
+        else:
+            coinbase.vout[0].nValue += spend.vout[0].nValue - 1  # all but one satoshi to fees
+            coinbase.rehash()
+            block = create_block(base_block_hash, coinbase, block_time, version=version)
+            tx = self.create_tx(spend, 0, 1, script)  # spend 1 satoshi
+            sign_tx(tx, spend)
+            self.add_transactions_to_block(block, [tx])
+            block.hashMerkleRoot = block.calc_merkle_root()
+        if solve:
+            block.solve()
+        self.tip = block
+        self.block_heights[block.sha256] = height
+        assert number not in self.blocks
+        self.blocks[number] = block
+        return block
+
+    def sync_blocks(self, blocks, success=True, reject_reason=None, force_send=False, reconnect=False, timeout=60):
+        """Sends blocks to test node. Syncs and verifies that tip has advanced to most recent block.
+
+        Call with success = False if the tip shouldn't advance to the most recent block."""
+        self.nodes[0].p2p.send_blocks_and_test(blocks, self.nodes[0], success=success, reject_reason=reject_reason, force_send=force_send, timeout=timeout, expect_disconnect=reconnect)
+
+        if reconnect:
+            self.reconnect_p2p(timeout=timeout)
+    # save the current tip so it can be spent by a later block
+    def save_spendable_output(self):
+        self.log.debug("saving spendable output %s" % self.tip.vtx[0])
+        self.spendable_outputs.append(self.tip)
+
+    # get an output that we previously marked as spendable
+    def get_spendable_output(self):
+        self.log.debug("getting spendable output %s" % self.spendable_outputs[0].vtx[0])
+        return self.spendable_outputs.pop(0).vtx[0]
+
+    def bootstrap_p2p(self, timeout=10):
+        """Add a P2P connection to the node.
+
+        Helper to connect and wait for version handshake."""
+        self.nodes[0].add_p2p_connection(P2PDataStore())
+        # We need to wait for the initial getheaders from the peer before we
+        # start populating our blockstore. If we don't, then we may run ahead
+        # to the next subtest before we receive the getheaders. We'd then send
+        # an INV for the next block and receive two getheaders - one for the
+        # IBD and one for the INV. We'd respond to both and could get
+        # unexpectedly disconnected if the DoS score for that error is 50.
+        self.nodes[0].p2p.wait_for_getheaders(timeout=timeout)
+
+    def init_coinbase(self):
+        self.bootstrap_p2p()  # Add one p2p connection to the node
+
+        # TODO: use self.nodes[0].get_deterministic_priv_key and b58decode_chk instead of generating my own blocks!
+
+        self.tip = None
+        self.blocks = {}
+        self.block_heights = {}
+        self.spendable_outputs = []
+
+        self.genesis_hash = int(self.nodes[0].getbestblockhash(), 16)
+        self.block_heights[self.genesis_hash] = 0
+
+        self.coinbase_key = ECKey()
+        self.coinbase_key.generate()
+        self.coinbase_pubkey = self.coinbase_key.get_pubkey().get_bytes()
+
+        # set initial blocktime to current time
+        self.start_time = int(1500000000)# int(time.time())
+        self.nodes[0].setmocktime=(self.start_time)
+
+        # generate mature coinbase to spend
+        NUM_BUFFER_BLOCKS_TO_GENERATE = 110
+        for i in range(NUM_BUFFER_BLOCKS_TO_GENERATE):
+            bn = self.next_block(i)
+            self.save_spendable_output()
+            self.sync_blocks([bn])
+
+        blockheight = self.nodes[0].getblockheader(blockhash=self.nodes[0].getbestblockhash())['height']
+
+        # collect spendable outputs now to avoid cluttering the code later on
+        self.coinbase_utxo = []
+        for i in range(NUM_OUTPUTS_TO_COLLECT):
+            self.coinbase_utxo.append(self.get_spendable_output())
+        self.coinbase_index = 0
+
+        self.nodes[0].generate(33)
+
+    def fund(self, tx, spend_tx, amount):
+        assert self.coinbase_index < NUM_OUTPUTS_TO_COLLECT
+        assert amount >= FEE_AMOUNT
+
+        fund_tx = self.coinbase_utxo[self.coinbase_index]
+        assert fund_tx != None
+        self.coinbase_index+=1
+        fund_key = self.coinbase_key
+        outIdx = 0 
+
+        #   update vin and witness to spend a specific update tx (skipped for setup tx)
+        if spend_tx != None:
+            tx.AddWitness(spend_tx)
+
+        #   pay change to new p2pkh output, TODO: should use p2wpkh
+        change_key = ECKey()
+        change_key.generate()
+        change_pubkey = change_key.get_pubkey().get_bytes()
+        change_script_pkh = CScript([OP_0, hash160(change_pubkey)])
+        change_amount = fund_tx.vout[0].nValue - amount
+
+        #   add new funding input and change output
+        tx.vin.append(CTxIn(COutPoint(fund_tx.sha256, 0), b""))
+        tx.vout.append(CTxOut(change_amount, change_script_pkh))
+
+        #   pay fee from spend_tx w/change output (assumed to be last txin)
+        inIdx = len(tx.vin)-1
+        
+        #   sign the tx fee input w/change output
+        scriptPubKey = bytearray(fund_tx.vout[outIdx].scriptPubKey)
+        (sighash, err) = SignatureHash(fund_tx.vout[0].scriptPubKey, tx, inIdx, SIGHASH_ALL)
+        sig = fund_key.sign_ecdsa(sighash) + bytes(bytearray([SIGHASH_ALL]))
+        tx.vin[inIdx].scriptSig = CScript([sig])
+
+        #   update the hash of this transaction
+        tx.rehash()
+
+        return (change_key, change_amount)
+
+    def commit(self, tx, error_code=None, error_message=None):
+        #   update hash
+        tx.rehash()
+
+        #   confirm it in the mempool
+        tx_hex = bytes_to_hex_str(tx.serialize_with_witness())
+        if error_code is None or error_message is None:
+            txid = self.nodes[0].sendrawtransaction(tx_hex, True)
+        else:
+            txid = assert_raises_rpc_error(error_code, error_message, self.nodes[0].sendrawtransaction, tx_hex)
+        return txid
+            
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = True
+        self.extra_args = [[]]
+
+    def run_test(self):
+        # create some coinbase txs to spend
+        self.init_coinbase()
+
+if __name__ == '__main__':
+    SimulateL2Tests().main()

remyers/bitcoin

test/functional/simulate_eltoo.py
@@ -1,0 +1,242 @@
+#!/usr/bin/env python3
+# Copyright (c) 2015-2019 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""
+Simulation tests for eltoo payment channel update scheme
+"""
+
+import copy
+from test_framework.blocktools import (
+    create_block,
+    create_coinbase
+)
+from test_framework.key import ECKey, ECPubKey, SECP256K1_ORDER
+from test_framework.messages import (
+    COutPoint,
+    CScriptWitness,
+    CTransaction,
+    CTxIn,
+    CTxInWitness,
+    CTxOut,
+    ToHex
+)
+from test_framework.mininode import P2PDataStore
+from test_framework.script import (
+    CScript,
+    CScriptNum,
+    OP_0,
+    OP_1, 
+    OP_2,
+    OP_2DUP,
+    OP_3DUP,
+    OP_2DROP,
+    OP_CHECKLOCKTIMEVERIFY, 
+    OP_CHECKMULTISIG,
+    OP_CHECKMULTISIGVERIFY,
+    OP_CHECKSEQUENCEVERIFY,
+    OP_CHECKSIG,
+    OP_CHECKSIGVERIFY,
+    OP_DROP,
+    OP_DUP,
+    OP_ELSE,
+    OP_ENDIF,
+    OP_EQUAL,
+    OP_EQUALVERIFY,
+    OP_FALSE,
+    OP_HASH160,
+    OP_IF,
+    OP_INVALIDOPCODE,
+    OP_NOTIF,
+    OP_RETURN,
+    OP_TRUE,
+    SIGHASH_ALL,
+    SIGHASH_ANYPREVOUT,
+    SIGHASH_NONE,
+    SIGHASH_SINGLE,
+    SIGHASH_ALLINPUT,
+    SIGHASH_ANYONECANPAY,
+    SIGHASH_ANYPREVOUT,
+    SIGHASH_NOINPUT,
+    SIGHASH_ANYPREVOUTANYSCRIPT,
+    SegwitVersion1SignatureHash,
+    SignatureHash,
+    hash160,
+    hash256,
+    sha256
+)
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_raises_rpc_error
+)
+import time
+import random
+
+NUM_OUTPUTS_TO_COLLECT = 33
+CSV_DELAY = 20
+DUST_LIMIT = 800
+FEE_AMOUNT = 1000
+CHANNEL_AMOUNT = 1000000
+RELAY_FEE = 100
+NUM_SIGNERS = 2
+CLTV_START_TIME = 500000000
+INVOICE_TIMEOUT = 3600 # 60 minute
+BLOCK_TIME = 600 # 10 minutes
+
+def int_to_bytes(x) -> bytes:
+    return x.to_bytes((x.bit_length() + 7) // 8, 'big')
+
+class SimulateL2Tests(BitcoinTestFramework):
+
+    def next_block(self, number, additional_coinbase_value=0, script=CScript([OP_TRUE]), solve=True, *, version=1):
+        if self.tip is None:
+            base_block_hash = self.genesis_hash
+            block_time = int(self.start_time) + 1
+        else:
+            base_block_hash = self.tip.sha256
+            block_time = self.tip.nTime + 1
+        # First create the coinbase
+        height = self.block_heights[base_block_hash] + 1
+        coinbase = create_coinbase(height, self.coinbase_pubkey)
+        coinbase.vout[0].nValue += additional_coinbase_value
+        coinbase.rehash()
+        block = create_block(base_block_hash, coinbase, block_time, version=version)
+        if solve:
+            block.solve()
+        self.tip = block
+        self.block_heights[block.sha256] = height
+        assert number not in self.blocks
+        self.blocks[number] = block
+        return block
+
+    def send_blocks(self, blocks, success=True, reject_reason=None, force_send=False, reconnect=False, timeout=60):
+        """Sends blocks to test node. Syncs and verifies that tip has advanced to most recent block.
+
+        Call with success = False if the tip shouldn't advance to the most recent block."""
+        self.nodes[0].p2p.send_blocks_and_test(blocks, self.nodes[0], success=success, reject_reason=reject_reason, force_send=force_send, timeout=timeout, expect_disconnect=reconnect)
+
+        if reconnect:
+            self.reconnect_p2p(timeout=timeout)
+    # save the current tip so it can be spent by a later block
+    def save_spendable_output(self):
+        self.log.debug("saving spendable output %s" % self.tip.vtx[0])
+        self.spendable_outputs.append(self.tip)
+
+    # get an output that we previously marked as spendable
+    def get_spendable_output(self):
+        self.log.debug("getting spendable output %s" % self.spendable_outputs[0].vtx[0])
+        return self.spendable_outputs.pop(0).vtx[0]
+
+    def bootstrap_p2p(self, timeout=10):
+        """Add a P2P connection to the node.
+
+        Helper to connect and wait for version handshake."""
+        self.nodes[0].add_p2p_connection(P2PDataStore())
+        # We need to wait for the initial getheaders from the peer before we
+        # start populating our blockstore. If we don't, then we may run ahead
+        # to the next subtest before we receive the getheaders. We'd then send
+        # an INV for the next block and receive two getheaders - one for the
+        # IBD and one for the INV. We'd respond to both and could get
+        # unexpectedly disconnected if the DoS score for that error is 50.
+        self.nodes[0].p2p.wait_for_getheaders(timeout=timeout)
+
+    def init_coinbase(self):
+        self.bootstrap_p2p()  # Add one p2p connection to the node
+
+        # TODO: use self.nodes[0].get_deterministic_priv_key and b58decode_chk instead of generating my own blocks!
+
+        self.tip = None
+        self.blocks = {}
+        self.block_heights = {}
+        self.spendable_outputs = []
+
+        self.genesis_hash = int(self.nodes[0].getbestblockhash(), 16)
+        self.block_heights[self.genesis_hash] = 0
+
+        self.coinbase_key = ECKey()
+        self.coinbase_key.generate()
+        self.coinbase_pubkey = self.coinbase_key.get_pubkey().get_bytes()
+
+        # set initial blocktime to current time
+        self.start_time = int(1500000000)# int(time.time())
+        self.nodes[0].setmocktime=(self.start_time)
+
+        # generate mature coinbase to spend
+        NUM_BUFFER_BLOCKS_TO_GENERATE = 110
+        for i in range(NUM_BUFFER_BLOCKS_TO_GENERATE):
+            bn = self.next_block(i)
+            self.save_spendable_output()
+            self.send_blocks([bn])
+
+        blockheight = self.nodes[0].getblockheader(blockhash=self.nodes[0].getbestblockhash())['height']
+
+        # collect spendable outputs now to avoid cluttering the code later on
+        self.coinbase_utxo = []
+        for i in range(NUM_OUTPUTS_TO_COLLECT):
+            self.coinbase_utxo.append(self.get_spendable_output())
+        self.coinbase_index = 0
+
+        self.nodes[0].generate(33)
+
+    def fund(self, tx, spend_tx, amount):
+        assert self.coinbase_index < NUM_OUTPUTS_TO_COLLECT
+        assert amount >= FEE_AMOUNT
+
+        fund_tx = self.coinbase_utxo[self.coinbase_index]
+        assert fund_tx != None
+        self.coinbase_index+=1
+        fund_key = self.coinbase_key
+        outIdx = 0 
+
+        #   update vin and witness to spend a specific update tx (skipped for setup tx)
+        if spend_tx != None:
+            tx.AddWitness(spend_tx)
+
+        #   pay change to new p2pkh output, TODO: should use p2wpkh
+        change_key = ECKey()
+        change_key.generate()
+        change_pubkey = change_key.get_pubkey().get_bytes()
+        change_script_pkh = CScript([OP_0, hash160(change_pubkey)])
+        change_amount = fund_tx.vout[0].nValue - amount
+
+        #   add new funding input and change output
+        tx.vin.append(CTxIn(COutPoint(fund_tx.sha256, 0), b""))
+        tx.vout.append(CTxOut(change_amount, change_script_pkh))
+
+        #   pay fee from spend_tx w/change output (assumed to be last txin)
+        inIdx = len(tx.vin)-1
+        
+        #   sign the tx fee input w/change output
+        scriptPubKey = bytearray(fund_tx.vout[outIdx].scriptPubKey)
+        (sighash, err) = SignatureHash(fund_tx.vout[0].scriptPubKey, tx, inIdx, SIGHASH_ALL)
+        sig = fund_key.sign_ecdsa(sighash) + bytes(bytearray([SIGHASH_ALL]))
+        tx.vin[inIdx].scriptSig = CScript([sig])
+
+        #   update the hash of this transaction
+        tx.rehash()
+
+        return (change_key, change_amount)
+
+    def commit(self, tx, error_code=None, error_message=None):
+        #   update hash
+        tx.rehash()
+
+        #   confirm it is in the mempool
+        tx_hex = ToHex(tx)
+        if error_code is None or error_message is None:
+            txid = self.nodes[0].sendrawtransaction(tx_hex)
+        else:
+            txid = assert_raises_rpc_error(error_code, error_message, self.nodes[0].sendrawtransaction, tx_hex)
+        return txid
+            
+    def set_test_params(self):
+        self.num_nodes = 1
+        self.setup_clean_chain = True
+        self.extra_args = [[]]
+
+    def run_test(self):
+        # create some coinbase txs to spend
+        self.init_coinbase()
+
+if __name__ == '__main__':
+    SimulateL2Tests().main()